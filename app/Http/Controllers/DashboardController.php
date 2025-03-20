6ed7e6a561883b88401ca1e8916864e6686a5222
arslankhan0123 <arslan.devsspace@gmail.com>
2025-03-20 23:10:08 +0500

    working on campaign

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -40,8 +40,10 @@ class DashboardController extends Controller
                 $data = $this->buildingRepo->all($request);
                 return view('sec_dashboard', compact('data'));
             } else if (Auth::user()->role()->first()->name == 'staff') {
-                $data = $this->buildingRepo->all($request);
-                return view('sec_dashboard', compact('data'));
+                // $data = $this->buildingRepo->all($request);
+                $data = $this->dashboardRepo->getData($request);
+                return view('dashboard', compact('data'));
+                // return view('sec_dashboard', compact('data'));
             }
         } catch (Exception $exception) {
             return redirect()->back()->with('error', 'Failed.' . $exception->getMessage());
