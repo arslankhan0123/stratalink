2502775ce0c6e7d1dc48c86c3c5d42fad43f2ca1
arslankhan0123 <arslan.devsspace@gmail.com>
2025-05-26 22:07:03 +0500

    working on video shorts

--- app/Http/Controllers/CallLogsController.php
+++ app/Http/Controllers/CallLogsController.php
@@ -81,8 +81,7 @@ class CallLogsController extends Controller
     {
         try {
             $request->validate([
-                'audio_attachments' => 'nullable|array',
-                'audio_attachments.*' => 'file|mimes:mp3,wav|max:10240', // Each file must meet the rules
+                'audio_attachment' => 'nullable|file|mimes:mp3,wav|max:10240',
             ]);
             $data = $this->callLogRepo->store($request);
             if ($request->send_email == 'yes') {
@@ -118,7 +117,7 @@ class CallLogsController extends Controller
                 //     'token' => $id_token,
                 //     'updated_at' => now(),
                 // ]);
-
+                
                 DB::table('call_logs')->where('id', $data['id'])->update([
                     'strata_manager_email_sent' => 'yes',
                     'updated_at' => now(),
@@ -135,7 +134,7 @@ class CallLogsController extends Controller
                 //     'token' => $id_token,
                 //     'updated_at' => now(),
                 // ]);
-
+                
                 DB::table('call_logs')->where('id', $data['id'])->update([
                     'contractor_details_email_sent' => 'yes',
                     'updated_at' => now(),
